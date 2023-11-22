193face035b6c488ab78d60ca68be1fc968b4e0f
Adriano Sudário <sudario.adriano@gmail.com>
2023-11-22 16:24:26 -0300

    Encrypted with password save and load added

--- serializable.gd
+++ serializable.gd
@@ -33,11 +33,34 @@ func save_json(path: String, json_indentation := "\t", sort_json_keys := false):
 	var index_file = FileAccess.open(path, FileAccess.WRITE)
 	var json_string = JSON.stringify(get_dictionary(), json_indentation, sort_json_keys, true)
 	index_file.store_string(json_string)
+	index_file.close()
 
 static func load_from_json(path: String) -> Serializable:
 	if not FileAccess.file_exists(path):
 		return null
 	
 	var json_content = FileAccess.get_file_as_string(path)
+	var dictionary = JSON.parse_string(json_content)
+	return dict_to_inst(dictionary)
+
+func save_encrypted(path: String, password:String):
+	var index_file = FileAccess.open_encrypted_with_pass(path, FileAccess.WRITE, password)
+	
+	if index_file == null:
+		push_error("couldn't save encrypted file")
+		return
+	
+	var json_string = JSON.stringify(get_dictionary(), "", false, true)
+	index_file.store_string(json_string)
+	index_file.close()
+
+static func load_from_encrypted(path: String, password:String) -> Serializable:
+	var index_file = FileAccess.open_encrypted_with_pass(path, FileAccess.READ, password)
+	
+	if index_file == null:
+		return null
+	
+	var json_content = index_file.get_as_text()
+	index_file.close()
 	var dictionary = JSON.parse_string(json_content)
 	return dict_to_inst(dictionary)
