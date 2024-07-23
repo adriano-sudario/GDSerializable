1ee9639384b65bbcc7b17024c9925b1b02060c7e
Adriano Sudário <sudario.adriano@gmail.com>
2024-07-23 09:47:50 -0300

    Update serializable.gd

--- serializable.gd
+++ serializable.gd
@@ -1,35 +1,88 @@
 class_name Serializable
 
-func get_dictionary():
+static func get_obj_properties(obj) -> Array:
+	var properties:Array = []
+	
+	if typeof(obj) == TYPE_DICTIONARY:
+		obj = dict_to_inst(obj)
+	
+	var property_list:Array = obj.get_property_list()
+	
+	for property in property_list:
+		if property.usage != PROPERTY_USAGE_SCRIPT_VARIABLE:
+			continue
+		
+		properties.append(property)
+	
+	return properties
+
+func get_properties() -> Array:
+	var properties:Array = []
+	var property_list:Array = self.get_property_list()
+	
+	for property in property_list:
+		if property.usage != PROPERTY_USAGE_SCRIPT_VARIABLE:
+			continue
+		
+		properties.append(property)
+	
+	return properties
+
+func get_fixed_save_value(value):
+	if Serializable.is_native_object(value):
+		value = var_to_str(value)
+	
+	return value
+
+static func is_native_object(value) -> bool:
+	var value_type = typeof(value)
+	
+	return not (value_type == TYPE_NIL or value_type == TYPE_BOOL \
+		or value_type == TYPE_INT or value_type == TYPE_FLOAT \
+		or value_type == TYPE_STRING)
+
+static func fix_values_after_load(loaded_obj) -> void:
+	if not is_native_object(loaded_obj):
+		return
+	
+	var variables:Array = Serializable.get_obj_properties(loaded_obj)
+	
+	for variable in variables:
+		if variable.type == TYPE_ARRAY:
+			for i in loaded_obj[variable.name].size():
+				Serializable.fix_values_after_load(loaded_obj[variable.name][i])
+		else:
+			if variable.type != TYPE_STRING and typeof(loaded_obj[variable.name]) == TYPE_STRING:
+				loaded_obj[variable.name] = str_to_var(loaded_obj[variable.name])
+
+func get_dictionary() -> Dictionary:
 	var dictionary:Dictionary = {}
 	var instance = inst_to_dict(self)
 	dictionary["@subpath"] = instance["@subpath"]
 	dictionary["@path"] = instance["@path"]
-	var variables:Array = []
-	var properties:Array = self.get_property_list()
-	
-	for property in properties:
-		if property.usage != PROPERTY_USAGE_SCRIPT_VARIABLE:
-			continue
-		
-		variables.append(property)
+	var variables:Array = get_properties()
 	
 	for variable in variables:
+		var value = self[variable.name]
+		
 		if variable.type == TYPE_ARRAY:
 			dictionary[variable.name] = []
+			
 			for i in self[variable.name].size():
-				if typeof(self[variable.name][i]) == TYPE_OBJECT:
-					dictionary[variable.name].append(self[variable.name][i].get_dictionary())
+				var array_value = self[variable.name][i]
+				
+				if typeof(array_value) == TYPE_OBJECT and array_value.has_method("get_dictionary"):
+					dictionary[variable.name].append(array_value.get_dictionary())
 				else:
-					dictionary[variable.name].append(self[variable.name][i])
-		elif variable.type == TYPE_OBJECT:
-			dictionary[variable.name] = self[variable.name].get_dictionary()
+					dictionary[variable.name].append(get_fixed_save_value(array_value))
+		elif variable.type == TYPE_OBJECT and value.has_method("get_dictionary"):
+			dictionary[variable.name] = value.get_dictionary()
 		else:
-			dictionary[variable.name] = self[variable.name]
+			dictionary[variable.name] = get_fixed_save_value(value)
 	
 	return dictionary
 
-func save_json(path: String, json_indentation := "\t", sort_json_keys := false):
+func save_json(path: String, json_indentation := "\t", sort_json_keys := false) -> void:
 	var index_file = FileAccess.open(path, FileAccess.WRITE)
 	var json_string = JSON.stringify(get_dictionary(), json_indentation, sort_json_keys, true)
 	index_file.store_string(json_string)
@@ -41,9 +94,11 @@ static func load_from_json(path: String) -> Serializable:
 	
 	var json_content = FileAccess.get_file_as_string(path)
 	var dictionary = JSON.parse_string(json_content)
-	return dict_to_inst(dictionary)
+	var loaded_dictionary = dict_to_inst(dictionary)
+	Serializable.fix_values_after_load(loaded_dictionary)
+	return loaded_dictionary
 
-func save_encrypted(path: String, password:String):
+func save_encrypted(path: String, password:String) -> void:
 	var index_file = FileAccess.open_encrypted_with_pass(path, FileAccess.WRITE, password)
 	
 	if index_file == null:
@@ -63,4 +118,6 @@ static func load_from_encrypted(path: String, password:String) -> Serializable:
 	var json_content = index_file.get_as_text()
 	index_file.close()
 	var dictionary = JSON.parse_string(json_content)
-	return dict_to_inst(dictionary)
+	var loaded_dictionary = dict_to_inst(dictionary)
+	Serializable.fix_values_after_load(loaded_dictionary)
+	return loaded_dictionary
